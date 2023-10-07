a6222bb277f0078ceea529105206e59ccb15c643
Shubham <shubhamdhingra9896@gmail.com>
2023-10-07 13:41:45 +0530

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+         <div>
+            <h1> TechyStar</h1>
+            <p>@all right reserved</p>
+         </div>
+         <div>
+            <h5>Follow us</h5>
+            <div> <a href="https://www.instagram.com/shubham___dhingra/ " target="blank" >Instagram</a>
+            <a href="https://www.youtube.com/channel/UC4UdBiDFy66LvnoCI8T8wMA" target="blank">Youtube</a>
+            <a href="https://github.com/Shubhamdhingra1" target="blank" >Github</a>
+            </div>
+         </div>
+    </footer>
+   
+  )
+}
+
+export default Footer
